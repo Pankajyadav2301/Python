29d0c2349ea75ac265a09b23aac6dcfa628ec98e
Pankaj Yadav <yadavn191.py@gmail.com>
2026-06-28 07:49:19 +0530

    Update main.py

--- Exercise 1.2/main.py
+++ Exercise 1.2/main.py
@@ -1,5 +1,5 @@
 # Fix the bugs in the folowing code
-print(String exercises")
-print("To concate Strings "+" symbol is used.")
-  print("e. g. print("Learning " + "Python")")
-print(("New line can be inserted using a backslash and n.")
+print("String exercises")
+print('To concate Strings "+" symbol is used.')
+print('e. g. print("Learning " + "Python")')
+print("New line can be inserted using a backslash and n.")
